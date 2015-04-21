1e1ac8a495fa546250e79d1bb3d82ee11624db9c
xiejinlong <303182519@qq.com>
2015-04-21 16:34:15 +0800

    删除dist文件夹

--- root/dist/modules/b/b.js
+++
@@ -1,6 +0,0 @@
-dwfis.define("b", function(require, exports, module){
-function b (argument) {
-	console.log('b');
-}
-module.exports= b;
-});
--- root/dist/pkg/page_map_0.js
+++
@@ -1,8 +0,0 @@
-require.resourceMap({
-    "res": {
-        "b": {
-            "url": "http://huyafed.yy.com/dist/modules/b/b.js"
-        }
-    },
-    "pkg": {}
-});
