02a89684673e981525705b7dd2644e0058b335e9
xiejinlong <303182519@qq.com>
2015-08-04 09:38:46 +0800

    修改配置

--- root/src/fis-dev.js
+++ root/src/fis-dev.js
@@ -1,4 +1,4 @@
-var configData = require('./fis-conf/fis-conf-com');
+var configData = require('./fis-conf/fis-conf-com').getConfig();
 
 //开发设置
 configData.roadmap.domain = 'http://hd.huya.com/150722游民星空/dev';
