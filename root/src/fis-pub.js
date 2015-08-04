eaa9930b536d2b943f7f2ff99d756de9d3f9d644
xiejinlong <303182519@qq.com>
2015-08-04 09:34:49 +0800

    修改配置

--- root/src/fis-pub.js
+++ root/src/fis-pub.js
@@ -1,4 +1,4 @@
-var configData = require('./fis-conf/fis-conf-com').getConfig('pub');
+var configData = require('./fis-conf/fis-conf-com');
 
 //发布设置
 configData.roadmap.domain = 'http://hd.huya.com/150722游民星空/dev';
